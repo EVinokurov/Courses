21020380ecd6340f24bbb051871318adf245be24
KurinS <stepan.kurin@yandex.ru>
2019-05-02 23:22:51 +0300

    format line

--- src/main/java/ru/itis/CourseApplication.java
+++ src/main/java/ru/itis/CourseApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 @EnableScheduling
 @SpringBootApplication//(exclude = {SecurityAutoConfiguration.class})
 public class CourseApplication {
